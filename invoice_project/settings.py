53df98ff93176107577a65aa1542571819493fb7
ankushsinghrajput.main@gmail.com <ankushsinghrajput-git>
2025-03-06 00:22:48 +0530

    Configuration for vercel

--- invoice_project/settings.py
+++ invoice_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9qk+2$pqxs_y-2qm=kk+a4ow^#2a!xd5n#u1l8%uiz*m#=btni"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
 
 # Application definition
